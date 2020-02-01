de4350c120c707c01a352eff3760e981e564827f
Adam Weizman <adamweizman@gmail.com>
2020-02-01 13:52:42 -0500

    Connected captionBot and MainActivity. Awaiting xml files to be laid out.

--- app/src/main/java/com/example/capgen/captionBot.java
+++ app/src/main/java/com/example/capgen/captionBot.java
@@ -1,4 +1,4 @@
-// package com.journaldev.readfileslinebyline;
+package com.example.capgen;// package com.journaldev.readfileslinebyline;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,9 +9,9 @@ import java.util.Random;
 import java.util.*;
 
 public class captionBot{
-	public static void main(String[] args) {
+	public static void main(String[] input) {
 		// File file = new File ("Desktop/google-cloud-sdk/quotes.txt");
-		String inWord = "glass";
+		String[] inWord = input;
 		Random lineMaker = new Random();
 		BufferedReader reader;
 		Vector quotes = new Vector();
@@ -29,10 +29,12 @@ public class captionBot{
 				reader = new BufferedReader(new FileReader("quotes.txt"));
 				String line = reader.readLine();
 				while (line != null) {
-					if (line.contains(inWord)){
-						quotes.add(line);
+					for (int i = 0; i < inWord.length; i++){
+						if (line.contains(inWord[i])){
+							quotes.add(line);
+						}
+						line = reader.readLine();
 					}
-					line = reader.readLine();
 				}
 				reader.close();
 			} catch (IOException e) {
@@ -42,7 +44,7 @@ public class captionBot{
 		// Scanner myScan = new Scanner(file);
 
 		int hold = lineMaker.nextInt(quotes.size());
-		System.out.print(quotes.get(hold));
+		System.out.print(quotes.get(hold)); //FIXME: send as output to xml file
 
 
 	}
