8876e81790cb600b72b93d05e07046e67d9f37c3
Adam Weizman <adamweizman@gmail.com>
2020-02-02 09:59:54 -0500

    fianl

--- app/src/main/java/com/example/capgen/captionBot.java
+++ app/src/main/java/com/example/capgen/captionBot.java
@@ -2,36 +2,27 @@
 package com.example.capgen;
 
 import android.app.Activity;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalTime;
-import java.util.Random;
-import java.util.*;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.time.LocalDateTime;
+
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 import androidx.annotation.RequiresApi;
 
@@ -105,17 +96,22 @@ public class captionBot extends Activity {
 		buttonGenerate.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				main(newerTerms);
+				try {
+					main(newerTerms);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
 			}
 
 		});
 	}
-	public void main(String[] args) {
+	public void main(String[] args) throws FileNotFoundException {
 		// File file = new File ("Desktop/google-cloud-sdk/quotes.txt");
 		String inWord1 = args[0];
 		String inWord2 = args[1];
 		//Random lineMaker = new Random();
-		BufferedReader reader;
+		FileInputStream fis=new FileInputStream("res/quotes.txt");
+		Scanner sc=new Scanner(fis);
 		String[] quotes = {null, null};
 
 		// List<String> lines = Files.readAllLines(Paths.get("quotes.txt"));
@@ -128,29 +124,25 @@ public class captionBot extends Activity {
 
 
 		// BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader("quotes.txt"));
-			String line = reader.readLine();
-			int i = 0;
-			while (line != null) {
-				if (line.contains(inWord1) || line.contains(inWord2)) {
-					quotes[i] = line;
-					Log.d("Quotes:", line);
-				}
-				line = reader.readLine();
-				i++;
-				if (i==2){
-					break;
-				}
+		String line = sc.nextLine();
+		Log.d("Line 0: ", line);
+		int i = 0;
+		while (line != null) {
+			if (line.contains(inWord1) || line.contains(inWord2)) {
+				quotes[i] = line;
+				Log.d("Quotes:", line);
 			}
-
-			reader.close();
-
-			Log.d("Quote 0: ", quotes[0]);
-			Log.d("Quote 1: ", quotes[1]);
-		} catch (IOException e) {
-			e.printStackTrace();
+			line = sc.nextLine();
+			i++;
+			if (i==2){
+				break;
+			}
 		}
+
+		sc.close();
+
+		Log.d("Quote 0: ", quotes[0]);
+		Log.d("Quote 1: ", quotes[1]);
 
 		// Scanner myScan = new Scanner(file);
 		LocalTime decider = null;
